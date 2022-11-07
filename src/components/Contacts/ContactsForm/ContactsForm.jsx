d6aa724ac5e7fa5a94d7d427054e526b1722b22a
JuliaOraz <97658697+JuliaOraz@users.noreply.github.com>
2022-11-07 21:29:01 +0200

    Update ContactsForm.jsx

--- src/components/Contacts/ContactsForm/ContactsForm.jsx
+++ src/components/Contacts/ContactsForm/ContactsForm.jsx
@@ -71,7 +71,7 @@ export const ContactsForm = () => {
           name="name"
           value={name}
           onChange={onChangeForm}
-          inputProps={{ inputMode: 'numeric', pattern: '^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$', title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan" }}
+          inputProps={{ inputMode: 'numeric', pattern: '^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$', title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d\'Artagnan" }}
           required
           sx={{
             mb: 2,
